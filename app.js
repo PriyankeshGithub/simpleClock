560ad49d454862f365d081bda6350c0e657f7164
PriyankeshGithub <priyankeshshivam@gmail.com>
2022-08-19 00:28:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const secondsHand = document.getElementById('seconds-hand')
+const minutesHand = document.getElementById('minutes-hand')
+const hoursHand = document.getElementById('hours-hand')
+
+
+const sec = document.getElementById('digi-sec')
+const min = document.getElementById('digi-min')
+const hr = document.getElementById('digi-hour')
+
+function getTime()
+ {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const timeInterval  = 6
+
+    secondsHand.style.transform = "rotate("+ (seconds * timeInterval) + "deg)"
+    minutesHand.style.transform = "rotate("+ ( minutes * timeInterval + seconds / 10) + "deg)"
+    hoursHand.style.transform = "rotate("+ ( hours * 30 + minutes / 2) + "deg)"
+
+
+
+    sec.innerText=seconds +" sec"
+    min.innerHTML=minutes + " min"
+    hr.innerText=hours +" hr"
+    
+}
+setInterval(getTime,100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
